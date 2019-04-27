f6bbb746c8c0fdaca90751142e55b58ec245c526
Loup Peluso <loup.peluso@gmail.com>
2019-04-27 10:52:49 +0200

    Tests

---
+++ src/__tests__/commandHandlers.tests/rotateCommandHandler.test.ts
@@ -0,0 +1,27 @@
+import { buildGameState } from "../../__fixtures__/buildGameState";
+import { rotateCommandHandler } from "../../commandHandlers";
+import { East, South } from "../../utils/directions";
+import { IAdventurer } from "../../models";
+
+it("should call rotator on the adventurer", () => {
+    const gameState = buildGameState();
+    const mockRotator = jest.fn();
+    rotateCommandHandler(gameState, {
+        type: "ROTATE",
+        adventurerId: 2,
+        rotator: mockRotator,
+    });
+    expect(mockRotator).toBeCalledWith(East);
+});
+
+it("should return different state", () => {
+    const gameState = buildGameState();
+    const newOrientation = South;
+    const mockRotator = jest.fn().mockReturnValue(newOrientation);
+    const newState = rotateCommandHandler(gameState, {
+        type: "ROTATE",
+        adventurerId: 2,
+        rotator: mockRotator,
+    });
+    expect((newState.objects.get(2) as IAdventurer).orientation).toBe(newOrientation);
+});
