620869573dc572a11c69ddbc22d887a966c1f01d
Loup Peluso <loup.peluso@gmail.com>
2019-05-07 00:19:23 +0200

    Rendering

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 import * as yargs from "yargs";
 import { run } from "./run";
+import { render } from "./display";
 
 const args = yargs.option("f", {
     alias: "filename",
@@ -14,4 +15,6 @@ if (!fs.existsSync(filename)) {
     throw new Error(`File ${filename} does not exist.`);
 }
 
-run(fs.createReadStream(filename));
+run(fs.createReadStream(filename))
+    .then(render)
+    .catch(console.error);
